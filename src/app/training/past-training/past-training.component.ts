9be7632d8da308176c09a67e5c4ec63df0612bc0
Kathreen Fenchenko <fev3@mail.ru>
2020-10-28 17:27:00 +0100

    sorting, filtering, pagination  added

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import {MatPaginator} from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort:MatSort
+  @ViewChild(MatPaginator) paginator: MatPaginator
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state']
   dataSource = new MatTableDataSource<Exercise>()
   constructor(private trainingService: TrainingService ) { }
 
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCanceledExerceses()
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
+  }
+
+  doFilter(filterValue:string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase()
   }
 
 }
